12dbc5a8745d577d7c58651a9a41579b1fd939b2
varshinipriya8489-debug <varshinipriya8489@gmail.com>
2025-08-06 14:02:26 +0530

    Add files via upload

---
+++ exp.6.py
@@ -0,0 +1,30 @@
+OPERATORS=set(['+','-','*','/','(',')','^'])
+PRIORITY={'+':1,'-':1,'*':2,'/':2,'^':3}
+def infix_to_postfix(expression):
+    stack=[]
+    output=''
+    for ch in expression:
+        if ch not in OPERATORS:
+            output+=ch
+        elif ch=='(':
+            stack.append('(')
+        elif ch==')':
+            while stack and stack[-1]!='(':
+                output+=stack.pop()
+            stack.pop()
+        else:
+            while stack and stack[-1]!='('and PRIORITY[ch]<=PRIORITY[stack[-1]]:
+                output+=stack.pop()
+            stack.append(ch)
+    while stack:
+        output+=stack.pop()
+    return output
+expression=input('enter infix  expression:')
+print('infix expression:',expression)
+print('postfix expression:',infix_to_postfix(expression))
+            
+            
+            
+            
+            
+        
